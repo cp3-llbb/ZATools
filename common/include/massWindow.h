6356695573fa8381ddf041aace5467629a92d533
alesaggio <saggio.alessia@gmail.com>
2018-09-26 14:56:12 +0200

    remove isOutOfEllipse function

--- common/include/massWindow.h
+++ common/include/massWindow.h
@@ -21,7 +21,6 @@ class massWindow {
         pair_d applyGlobalTransformation(pair_d ref, pair_d point);
         pair_d applyLocalTranformation(pair_d point);
         bool isInEllipse(double center_x, double center_y, double size, double point_x, double point_y);
-        bool isOutOfEllipse(double center_x, double center_y, double size, double point_x, double point_y);
         double isInEllipse_noSize(double center_x, double center_y, double point_x, double point_y);
 
     private:
