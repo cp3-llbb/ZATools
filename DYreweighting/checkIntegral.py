5b4f5ca3fa8634ab217a12c413418c75b539362a
alesaggio <saggio.alessia@gmail.com>
2018-10-23 11:28:26 +0200

    add checkIntegral.py

---
+++ DYreweighting/checkIntegral.py
@@ -0,0 +1,41 @@
+#! /bin/env python
+
+import sys, os, json
+import copy
+import numpy as np
+import glob
+import re
+import os.path
+import ROOT
+from ROOT import TCanvas, TPad, TLine
+from ROOT import kBlack, kBlue, kRed
+
+from getHisto import getHisto
+import argparse
+
+
+def main():
+
+    file_path_DYreweighted = '/home/ucl/cp3/asaggio/scratch/CMSSW_8_0_30/src/cp3_llbb/ZATools/factories_ZA/DYreweighted_onlymjj_updown_forDY'
+    file_path_DYreweighted_withmlljj = '/home/ucl/cp3/asaggio/scratch/CMSSW_8_0_30/src/cp3_llbb/ZATools/factories_ZA/DYreweighted_mjj_and_mlljj_updown_pol6_forDY/'
+    file_path_data = '/home/ucl/cp3/asaggio/scratch/CMSSW_8_0_30/src/cp3_llbb/ZATools/factories_ZA/DYreweighted_onlymjj_updown_fordata'
+    file_path_DYnoreweighted = "/home/ucl/cp3/asaggio/scratch/CMSSW_8_0_30/src/cp3_llbb/ZATools/factories_ZA/noDYreweighted_forDY"
+    file_path_DYreweighteddividedBy1p009577814 = "/home/ucl/cp3/asaggio/scratch/CMSSW_8_0_30/src/cp3_llbb/ZATools/factories_ZA/DYreweighted_onlymjj_updown_dividedBy1p009577814_forDY"
+
+    #prefix can be either "jj_M" or "lljj_M"
+    mjj_MC = getHisto(file_path_DYreweighted, prefix="jj_M", isData=False)
+    mjj_data = getHisto(file_path_data, prefix="jj_M", isData=True)
+    mjj_MCnoreweighted = getHisto(file_path_DYnoreweighted, prefix="jj_M", isData=False)
+    mjj_MCreweighteddividedBy1p009577814 = getHisto(file_path_DYreweighteddividedBy1p009577814, prefix="jj_M", isData=False)
+    
+    mlljj_MCnoreweighted = getHisto(file_path_DYnoreweighted, prefix="lljj_M", isData=False)
+    mlljj_MCreweighted_withmlljj = getHisto(file_path_DYreweighted_withmlljj, prefix="lljj_M", isData=False)
+
+    print "Integral mlljj_MC with DY reweighted: ", mlljj_MCreweighted_withmlljj.Integral()
+    print "Integral mlljj_MC no DY reweighted: ", mlljj_MCnoreweighted.Integral()
+    print "mlljj reweighted/non-reweighted: ", mlljj_MCreweighted_withmlljj.Integral()/mlljj_MCnoreweighted.Integral()
+        
+##main
+if __name__ == "__main__":
+    ROOT.gROOT.SetBatch(True)
+    main()
